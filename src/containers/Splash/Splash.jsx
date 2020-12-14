10ac167dc78ab7aab8ef89d358ef45342859f2d3
Nathan Jen <njen@andrew.cmu.edu>
2020-12-14 17:31:51 -0500

    updated splash style

--- src/containers/Splash/Splash.jsx
+++ src/containers/Splash/Splash.jsx
@@ -1,7 +1,24 @@
 import React, { useContext } from "react"
-import styled from "styled-components"
+import styled, { keyframes } from "styled-components"
 import ControlContext from '../../shared/control-context'
 import GoogleLogo from '../../assets/google-logo.png'
+
+export default function Splash () {
+  const {
+    loginUser,
+  } = useContext(ControlContext);
+  
+  return (
+    <LoginContainer>
+      <Title>Remote<Blue>HQ</Blue></Title>
+      <Subtitle>Virtual Collaboration Made Easy</Subtitle>
+      <Button id="LoginPage-login-button" onClick={() => loginUser()}>
+        <Img src={GoogleLogo} />
+        Sign In with Google
+      </Button>
+    </LoginContainer>
+  )
+}
 
 const Title = styled.h1`
   font-size: 72px;
@@ -10,11 +27,25 @@ const Title = styled.h1`
   margin-top: 15vh;
 `
 
+const fadeIn = keyframes`
+  0% {
+    opacity:0;
+    transform: translateY(0);
+  }
+  100% {
+    opacity:1;
+    transform: translateY(-20px);
+  }
+`
+
 const Subtitle = styled.h4`
-  margin-top: 20px;
+  margin-top: 50px;
   font-size: 28px;
   text-align: center;
   font-weight: 300;
+  color: #5C677D;
+  animation: ${fadeIn} ease 2s;
+  transform: translateY(-20px);
 `
 
 const Button = styled.button`
@@ -28,6 +59,14 @@ const Button = styled.button`
   cursor: pointer;
   display: flex;
   align-items: center;
+  transition: all 0.3s ease-out;
+
+  &:hover {
+    transform: translateY(-5px);
+    transition-timing-function: ease-in;
+    transition: 0.5s;
+    box-shadow: 0px 15px 35px rgba(0, 0, 0, 0.25);
+  }
 `
 
 const Img = styled.img`
@@ -40,19 +79,7 @@ const LoginContainer = styled.div`
   width: 100%;
 `
 
-export default function Splash () {
-  const {
-    loginUser,
-  } = useContext(ControlContext);
-  
-  return (
-    <LoginContainer>
-      <Title>Remote<b>HQ</b></Title>
-      <Subtitle>Virtual Collaboration Made Easy</Subtitle>
-      <Button id="LoginPage-login-button" onClick={() => loginUser()}>
-        <Img src={GoogleLogo} />
-        Sign In with Google
-      </Button>
-    </LoginContainer>
-  )
-}
+const Blue = styled.span`
+  color: #0466C8;
+  font-weight: bold;
+`
